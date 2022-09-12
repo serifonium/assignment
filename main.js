e372c3d372e957817ad2c0293ff2ca6fce1cd1e7
Aiden Venter <aiden.venter@gmail.com>
2022-09-12 12:44:37 +1000

    footer

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ $(document).ready(function(){
 	$('.header').height($(window).height());
 })
 
+
+
+setInterval(()=>{
+	document.getElementById('footer').style.bottom = "3%";
+	console.log("ehehehehe")
+}, 100)
